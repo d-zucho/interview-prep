bd847fd1a69f5980bcde5e65d409053375f5bf04
D_Zucho <danny.nico.dev@outlook.com>
2025-04-08 12:41:49 -0700

    add home hero

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,54 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <section className='card-cta'>
+        <div className='flex items-center gap-6'>
+          <div className='flex flex-col gap-6 max-w-lg '>
+            <h2 className='text-2xl lg:text-[30px]'>
+              Get Interview Ready with AI-Powered Practice & Feedback
+            </h2>
+            <p className='text-[16px] lg:text-lg'>
+              Practice on real interview questions & get feedback
+            </p>
+            <Button asChild className='btn-primary max-sm:w-full'>
+              <Link href='/interview'>Start an Interview</Link>
+            </Button>
+          </div>
+          <div className=''>
+            <Image
+              src='/robot.png'
+              alt='robo-dude'
+              width={400}
+              height={400}
+              className='max-sm:hidden'
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2 className='text-2xl lg:text-[30px]'>Your Interviews</h2>
+        <div className='interviews-section'>
+          <p className='text-[16px] lg:text-lg'>
+            You haven&apos;t taken any interviews yet
+          </p>
+        </div>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2 className='text-2xl lg:text-[30px]'>Take an Interview</h2>
+        <div className='interviews-section'>
+          <p className='text-[16px] lg:text-lg'>
+            There are no interviews available
+          </p>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Home
