4c483e5ce257e15a21d6a0c5702bc300e4019e0a
D_Zucho <danny.nico.dev@outlook.com>
2025-04-10 06:23:30 -0700

    implement firebase auth & user db

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,18 +1,24 @@
 'use client'
 
 import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { Form } from '@/components/ui/form'
-import { Button } from './ui/button'
-import { Input } from './ui/input'
-import Image from 'next/image'
 import Link from 'next/link'
+import Image from 'next/image'
 import { toast } from 'sonner'
-import FormField from './FormField'
+import { auth } from '@/firebase/client'
+import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-type FormType = 'sign-up' | 'sign-in'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth'
+
+import { Form } from '@/components/ui/form'
+import { Button } from '@/components/ui/button'
+
+import { signIn, signUp } from '@/lib/actions/auth.action'
+import FormField from './FormField'
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -24,8 +30,8 @@ const authFormSchema = (type: FormType) => {
 
 const AuthForm = ({ type }: { type: FormType }) => {
   const router = useRouter()
+
   const formSchema = authFormSchema(type)
-  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -35,36 +41,71 @@ const AuthForm = ({ type }: { type: FormType }) => {
     },
   })
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     try {
       if (type === 'sign-up') {
+        const { name, email, password } = data
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        )
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        })
+
+        if (!result.success) {
+          toast.error(result.message)
+          return
+        }
+
         toast.success('Account created successfully. Please sign in.')
         router.push('/sign-in')
-      } else if (type === 'sign-in') {
+      } else {
+        const { email, password } = data
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        )
+
+        const idToken = await userCredential.user.getIdToken()
+        if (!idToken) {
+          toast.error('Sign in Failed. Please try again.')
+          return
+        }
+
+        await signIn({
+          email,
+          idToken,
+        })
+
         toast.success('Signed in successfully.')
         router.push('/')
       }
     } catch (error) {
-      // Handle error
-      console.error('Error submitting form:', error)
-      toast.error(`There was an error: ${error}`, {
-        description: 'Please try again later.',
-        duration: 3000,
-      })
+      console.log(error)
+      toast.error(`There was an error: ${error}`)
     }
   }
 
   const isSignIn = type === 'sign-in'
+
   return (
-    <div>
+    <div className='card-border lg:min-w-[566px]'>
       <div className='flex flex-col gap-6 card py-14 px-10'>
         <div className='flex flex-row gap-2 justify-center'>
-          <Image src={'/logo.svg'} alt='logo' width={32} height={38} />
+          <Image src='/logo.svg' alt='logo' height={32} width={38} />
           <h2 className='text-primary-100'>PrepWise</h2>
         </div>
 
-        <h3>Practice Job Interviews with AI</h3>
+        <h3>Practice job interviews with AI</h3>
 
         <Form {...form}>
           <form
@@ -73,39 +114,43 @@ const AuthForm = ({ type }: { type: FormType }) => {
           >
             {!isSignIn && (
               <FormField
-                name='name'
                 control={form.control}
+                name='name'
                 label='Name'
-                placeholder='Your name'
+                placeholder='Your Name'
+                type='text'
               />
             )}
+
             <FormField
-              name='email'
               control={form.control}
+              name='email'
               label='Email'
-              placeholder='johnsmith@example.com'
+              placeholder='Your email address'
               type='email'
             />
+
             <FormField
-              name='password'
               control={form.control}
+              name='password'
               label='Password'
-              placeholder='********'
+              placeholder='Enter your password'
               type='password'
             />
 
-            <Button type='submit' className='btn'>
-              {isSignIn ? 'Sign in' : 'Create an account'}
+            <Button className='btn' type='submit'>
+              {isSignIn ? 'Sign In' : 'Create an Account'}
             </Button>
           </form>
         </Form>
+
         <p className='text-center'>
           {isSignIn ? 'No account yet?' : 'Have an account already?'}
           <Link
             href={!isSignIn ? '/sign-in' : '/sign-up'}
             className='font-bold text-user-primary ml-1'
           >
-            {!isSignIn ? 'Sign in' : 'Sign up'}
+            {!isSignIn ? 'Sign In' : 'Sign Up'}
           </Link>
         </p>
       </div>
--- firebase/client.ts
+++ firebase/client.ts
@@ -1,7 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from 'firebase/app'
-import { getAuth } from 'firebase-admin/auth'
-import { getFirestore } from 'firebase-admin/firestore'
+import { getApp, getApps, initializeApp } from 'firebase/app'
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
